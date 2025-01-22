ac3282ed5dc8d6d574b749a331673601e30973da
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-23 00:41:32 +0100

    initial commit

---
+++ mongoose.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const messageSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+});
+
+const usernameSchema = mongoose.Schema({
+    username:{
+        type: String,
+        required: true
+    }
+        
+})
+
+
+const logMessages = mongoose.model("Messagelog", messageSchema);
+const logUsername = mongoose.model("Usernamelogs",usernameSchema);
+module.exports = logMessages;
---
+++ server.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const msgLog = require("./mongoose")
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+app.get(
+  "/api/messages",
+  async(req, res) => {
+    const storedMessages = await msgLog.find({}).sort({timestamp: 1});
+    console.log("Getting all messages" + storedMessages);
+    res.json(storedMessages);
+  }
+);
+
+app.post(
+  "/api/messages",
+  async (req, res) => {
+    try {
+      const message = await msgLog.create(req.body);
+      if (!message) {
+        return res.status(400).json({ error: "Text is required" });
+      }
+    }
+    catch (error) {
+      console.log(error.message)
+      res.status(404);
+    }
+  }
+);
+
+mongoose.
+connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("connected to mongodb");
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
+  
+});
