9717c6c2d46f74c302c65d902e064670d5de0ab1
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-23 20:40:21 +0100

    update packages and add catch statement

--- server.js
+++ server.js
@@ -3,13 +3,19 @@ const mongoose = require("mongoose");
 const msgLog = require("./mongoose")
 const express = require("express");
 const http = require("https");
-const { Server } = require("socket.io");
+const {Server} = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
 const CLIENT_URL = "https://nice-grass-0741fb703.4.azurestaticapps.net";
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors({
+  origin: CLIENT_URL,
+}));
 
 const io = new Server(server, {
   cors: {
@@ -27,20 +33,13 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(express.static("public"));
-app.use(express.json());
-app.use(cors({
-  origin: CLIENT_URL,
-}));
-
 app.get(
   "/api/messages",
-  async(req, res) => {
-    try{
+  async (req, res) => {
+    try {
       const storedMessages = await msgLog.find({}).sort({timestamp: 1});
       res.json(storedMessages);
-    }
-    catch (error){
+    } catch (error) {
       console.log(error);
     }
   }
@@ -52,12 +51,11 @@ app.post(
     try {
       const message = await msgLog.create(req.body);
       if (!message) {
-        return res.status(400).json({ error: "Text is required" });
+        return res.status(400).json({error: "Text is required"});
       }
       io.emit("message", message);
       res.json(message);
-    }
-    catch (error) {
+    } catch (error) {
       console.log(error.message)
       res.status(404);
     }
@@ -69,4 +67,5 @@ mongoose
   .then(() => {
     console.log("connected to mongodb");
     server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-  });
+  })
+  .catch((error) => console.log(error));
