07d7aaa172ec114987a2ee94fec5b6ecb0dbb91a
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-01-23 19:27:15 +0100

    fix websocket setup and cors

--- server.js
+++ server.js
@@ -2,31 +2,42 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const msgLog = require("./mongoose")
 const express = require("express");
-const https = require("https");
+const http = require("https");
 const { Server } = require("socket.io");
 const app = express();
-const server = https.createServer(app);
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
 
+const CLIENT_URL = "https://nice-grass-0741fb703.4.azurestaticapps.net";
+
 const io = new Server(server, {
   cors: {
-      origin: "https://nice-grass-0741fb703.4.azurestaticapps.net", // Frontend URL
-      methods: ["GET", "POST"],
+    origin: CLIENT_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
   },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors());
-
+app.use(cors({
+  origin: CLIENT_URL,
+}));
 
 app.get(
   "/api/messages",
   async(req, res) => {
     try{
       const storedMessages = await msgLog.find({}).sort({timestamp: 1});
-      console.log("Getting all messages" + storedMessages);
       res.json(storedMessages);
     }
     catch (error){
@@ -53,19 +64,9 @@ app.post(
   }
 );
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("disconnect", () => {
-      console.log("A user disconnected");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to mongodb");
+    server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   });
-});
-
-mongoose.
-connect(process.env.MONGO_URI)
-.then(() => {
-  console.log("connected to mongodb");
-  server.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
-  
-});
